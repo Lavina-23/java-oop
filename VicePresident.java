66caea240ecfccfb79123095ffda35eec44293f1
Lavina-23 <lavinakohai3227@gmail.com>
2024-01-04 12:10:09 +0700

    feat: overriding

--- VicePresident.java
+++ VicePresident.java
@@ -1,3 +1,7 @@
 public class VicePresident extends Manager {
-
+    // method overriding
+    // create same method from the parent class
+    void sayHello(String name) {
+        System.out.println("Hai " + name + ", my name is vp " + this.name);
+    }
 }
